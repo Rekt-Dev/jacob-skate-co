477f3389651e0ed979b36702cc4c5a368387dcb9
Rekt Dev <rektdevvv@gmail.com>
2026-06-08 13:30:36 +0300

    fix: use JacobLogoDark.png and remove invert filter so logo renders correctly

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/JacobLogo.png';
+import logo from '../assets/JacobLogoDark.png';
 
 const Logo: React.FC = () => (
   <div className="justifyCenter">
